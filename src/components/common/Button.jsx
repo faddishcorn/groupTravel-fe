7c13fc7847de5f2803cad99c18d7fa5cdd281201
siwoo <pty0714@naver.com>
2025-09-22 00:47:08 +0900

    버튼 컴포넌트

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,18 @@
+// Button Components
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    // basic style
+
+    // hover style
+`;
+
+function Button({ }) {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Button;
